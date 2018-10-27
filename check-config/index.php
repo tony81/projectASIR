61a672782e253ea080121309baac7cbc4f9c12f4
antonio <Antonio Garca>
2018-10-27 21:13:03 +0200

    last configuration and add php file check-config/index.php

---
+++ check-config/index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo '<pre><h3>';
+print_r($_SERVER['FOO']);
+echo '</h3></pre>';
+echo "<br/>";
+
+$enlace = mysqli_connect("database", "root", "mysql_wp", "mysql");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL!" . PHP_EOL."<br/><br/>";
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+mysqli_close($enlace);
+
+//print_r($_SERVER['FOO']);
+//phpinfo();
